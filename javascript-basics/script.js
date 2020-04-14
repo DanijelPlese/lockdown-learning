a4b3e974faa24dc2c78194c465696a059e8b789d
DanijelPlese <62547369+DanijelPlese@users.noreply.github.com>
2020-04-14 17:10:47 +0200

    ok
    ok

---
+++ javascript-basics/script.js
@@ -0,0 +1,25 @@
+
+/*
+const person = {
+    name : 'David',
+    age : 30,
+}
+
+
+if(person.name !== 'David') {
+    console.log('My name is not David');
+} else {
+    console.log('My name is David');
+}
+
+const hour = 21;
+
+if (hour > 6 && hour < 12) {
+    console.log('It\'s morning.');
+} else if (hour >= 12 && hour < 20) {
+    console.log('It\'s afternoon.');
+} else {
+    console.log('It\'s not daytime');
+}
+*/
+
---
+++ the-dom/script.js
@@ -0,0 +1,21 @@
+/*
+const paragraph = document.querySelector('p');
+
+console.log(paragraph.textContent);
+*/
+/*
+const item = document.querySelector('.item');
+
+item.classList.toggle('ok');
+
+console.log(item.classList);
+*/
+const image = document.querySelector('img');
+
+image.src = ​'https://crocoder.dev/logo.png'
+
+console.log(image);
+
+
+
+
